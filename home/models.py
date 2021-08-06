8438de8140ef1c12fd02ba005ecec8fba199ec0d
Crowdbotics <team@crowdbotics.com>
2021-08-06 09:45:35 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+
+
+class Home(models.Model):
+    "Generated Model"
+    distance = models.FloatField()
+    price = models.DecimalField(
+        max_digits=30,
+        decimal_places=10,
+    )
+    status = models.TextField()
